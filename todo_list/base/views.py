b461ba7f3cff77ca2f4ba893a66c95ab484e40c1
Gamattowi <p.romanczuk31@gmail.com>
2021-05-20 12:10:50 +0200

    add task list view

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Task
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def task_list(request):
+    tasks = Task.objects.all()
+    context = {'tasks': tasks}
+    return render(request, 'base/task_list.html', context)
+
+
