c1633df8a20f2bb0614ee36b5ea9eabdfb112410
Gamattowi <p.romanczuk31@gmail.com>
2021-05-27 19:59:03 +0200

    change text in home page

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from .models import Task
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
