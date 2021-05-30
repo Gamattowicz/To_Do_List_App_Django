ecc3733cf7b9d2b30d36dc5890e0469be4cbec74
Gamattowi <p.romanczuk31@gmail.com>
2021-05-30 14:46:56 +0200

    fix update and detail task page

--- todo_list/base/models.py
+++ todo_list/base/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
-    color = models.CharField(max_length=7, null=True)
+    color = models.CharField(max_length=7, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
