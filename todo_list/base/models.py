4e532951c4f8942b91b0fe1cdd2690a1f62b8c34
Gamattowi <p.romanczuk31@gmail.com>
2021-05-30 13:25:53 +0200

    add category model

--- todo_list/base/models.py
+++ todo_list/base/models.py
@@ -2,8 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    color = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{str(self.name)}'
+
+
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='task', null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='task_category', null=True)
     title = models.CharField(max_length=200)
     content = models.TextField()
     complete = models.BooleanField(default=False)
