825f715461b8e812ebe09edd6634c9a0538333de
Gamattowi <p.romanczuk31@gmail.com>
2021-05-31 18:51:08 +0200

    add deadline field to Task model

--- todo_list/base/models.py
+++ todo_list/base/models.py
@@ -21,6 +21,7 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     complete = models.BooleanField(default=False)
+    deadline = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
